86bf4f8175afce30d6e45f7e813ec5f31ed5aa43
jomlabs <mosetijeremy@gmail.com>
2025-09-22 12:04:02 -0500

    adds week 2 folder and contents

--- Week1/CodingChallenges/8_Loops/8_Loops/Program.cs
+++ Week1/CodingChallenges/8_Loops/8_Loops/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using System;
+using System.Text;
 using System.Collections.Generic;
 
 namespace _8_LoopsChallenge
@@ -20,7 +21,18 @@ namespace _8_LoopsChallenge
         /// <returns></returns>
         public static int UseFor(List<int> x)
         {
-            throw new NotImplementedException("UseFor() is not implemented yet.");
+            //throw new NotImplementedException("UseFor() is not implemented yet.");
+            int oddNumbers = 0;
+
+            for (int i = 0; i < x.Count; i++)
+            {
+                if (x[i] % 2 != 0)
+                {
+                    oddNumbers += 1;
+                }
+
+            }
+            return oddNumbers;
         }
 
         /// <summary>
@@ -31,8 +43,31 @@ namespace _8_LoopsChallenge
         /// <returns></returns>
         public static int UseForEach(List<object> x)
         {
-            throw new NotImplementedException("UseForEach() is not implemented yet.");
+            int count = 0;
+            foreach (var o in x)
+            {
+                if (o is int n && n % 2 == 0)
+                {
+                    count++;
+                }
+                else if (o is string s && int.TryParse(s, out int num) && num % 2 == 0)
+                {
+                    count++;
+                }
+                else if (o is double d && d % 2 == 0)
+                {
+                    count++;
+                }
+                else if (o is long l && l % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
+
+
+
 
         /// <summary>
         /// This method counts the multiples of 4 from the provided List<int>. 
@@ -42,7 +77,29 @@ namespace _8_LoopsChallenge
         /// <param name="x"></param>
         public static int UseWhile(List<int> x)
         {
-            throw new NotImplementedException("UseFor() is not implemented yet.");
+            //throw new NotImplementedException("UseFor() is not implemented yet.");
+            int count = 0;
+            int i = 0;
+
+            while (i < x.Count)
+
+            {
+                int num = x[i];
+
+                if (num == 1234)
+
+                {
+                    break;
+                }
+
+                if (num % 4 == 0)
+                {
+                    count++;
+                }
+                i++;
+
+            }
+            return count;
         }
 
         /// <summary>
@@ -53,7 +110,21 @@ namespace _8_LoopsChallenge
         /// <returns></returns>
         public static int UseForThreeFour(int[] x)
         {
-            throw new NotImplementedException("UseForThreeFour() is not implemented yet.");
+            //throw new NotImplementedException("UseForThreeFour() is not implemented yet.");
+
+            int count = 0;
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] % 3 == 0 && x[i] % 4 == 0)
+
+                {
+                    count += 1;
+                }
+
+
+            }
+            return count;
         }
 
         /// <summary>
@@ -64,7 +135,35 @@ namespace _8_LoopsChallenge
         /// <returns></returns>
         public static string LoopdyLoop(List<string>[] stringListArray)
         {
-            throw new NotImplementedException("LoopdyLoop() is not implemented yet.");
+            StringBuilder result = new StringBuilder();
+
+            if (stringListArray != null)
+            {
+                foreach (var list in stringListArray)
+                {
+                    if (list != null)
+                    {
+                        foreach (string s in list)
+                        {
+                            if (!string.IsNullOrEmpty(s))
+                            {
+                                if (result.Length > 0)
+                                {
+                                    result.Append(" ");
+                                }
+                                result.Append(s);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (result.Length > 0)
+            {
+                result.Append(" ");
+            }
+
+            return result.ToString();
         }
     }
 }
